b2cf99c23850e27a881272ec139242e8dcb9295d
blueyorange <blueyorange@gmail.com>
2021-01-24 17:18:00 +0000

    mark scheme reader working for MC

--- readMS.py
+++ readMS.py
@@ -16,3 +16,10 @@ imagelist = convert_from_path('June 2003 MS - Paper 1 CIE Physics IGCSE.pdf')
 im = imagelist[1]
 text = image_to_string(im)
 print(text)
+answerRegEx = re.compile('\d+\s[ABCDc]')
+
+matches = answerRegEx.findall(text)
+print(matches.sort())
+answers = {match.split(" ")[0]:match.split(" ")[1] for match in matches}
+answers = {i:answers[str(i)] for i in range(1,41)}
+print(answers)
